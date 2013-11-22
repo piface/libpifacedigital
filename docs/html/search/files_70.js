336f703ef45d49a89bb78ab03f2480d2e47d5213
Thomas Preston <thomasmarkpreston@gmail.com>
2013-11-22 08:51:09 +0000

    updated docs

--- docs/html/search/files_70.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pifacedigital_2eh',['pifacedigital.h',['../pifacedigital_8h.html',1,'']]]
-];
--- docs/html/search/pages_6c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['libpifacedigital_20_2d_20a_20simple_20c_20library_20for_20piface_20digital_2e',['libpifacedigital - A simple C library for PiFace Digital.',['../index.html',1,'']]]
-];
